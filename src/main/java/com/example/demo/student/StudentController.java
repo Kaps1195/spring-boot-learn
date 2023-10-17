0c0379095492cff3f95e88fa7e75abcebd2a1bca
Kapil Ramnani <kramnani95@gmail.com>
2023-10-17 13:08:06 +0800

    Dependency Injection with @ annotation (Beans/Service+Autowired) + GetMapping fix

--- src/main/java/com/example/demo/student/StudentController.java
+++ src/main/java/com/example/demo/student/StudentController.java
@@ -1,5 +1,7 @@
 package com.example.demo.student;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,10 +12,12 @@ import java.util.List;
 public class StudentController {
     private final StudentService studentService;
 
+    @Autowired
     public StudentController(StudentService studentService) {
         this.studentService = studentService;
     }
 
+    @GetMapping
     public List<Student> getStudents() {
         return studentService.getStudents();
     }
